fb97c7a60a09714a8cb932517fb94602d28c3260
Maxwell Heeschen <gorpmaster@gmail.com>
2017-02-24 02:35:34 -0600

    HW4: added the skeleton for the program

--- hw4/hw4.cpp
+++ hw4/hw4.cpp
@@ -8,15 +8,55 @@
 
 #include<iostream>
 #include<fstream>
+#include<cassert>
+#include<string>
 using namespace std;
 
-ifstream file1;
-ifstream file2;
+int readFile(int inputArray[], ifstream& instream);
+//Input:
+//      inputArray[] is the array to get the size of
+//      instream is the stream to use with numbers in it
+//Return:
+//      the size of inputArray[]
 
-int file1Next;
-int file2Next;
+int sort(int inputArray1[], int inputArray1_size, int inputArray2[], int inputArray2_size, int sorted[]);
+//Input:
+//      inputArray1[] is one of the arrays to be sorted
+//      inputArray1_size is the size of the first array
+//      inputArray2[] is second array to be sorted with the first
+//      inputArray2_size is the size of the second array
+//      sorted[] is the sorted array of both inputArray1[] and inputArray2[]
+//Return:
+//      the size of the sorted[] array
+//      change a premade array to be sorted
+
+void writeFile(int sorted[], int sorted_size);
+//Input:
+//      sorted[] is the sorted array of the two files
+//      sorted_size is the size of sorted[]
+//Return:
+//      makes a file with the sorted numbers in it
+
 
 const int MAX_ARRAY_SIZE = 1000;
-int sorted[MAX_ARRAY_SIZE];
 
-int Main() {
+int main() {
+    ifstream file1;
+    ifstream file2;
+    ofstream outFile;
+    int file1Next;
+    int file2Next;
+
+    return 0;
+}
+
+int readFile(int inputArray[], ifstream instream) {
+    return -99999;
+}
+
+int sort(int inputArray1[], int inputArray1_size, int inputArray2[], int inputArray2_size, int sorted[]) {
+    return -99999;
+}
+
+void writeFile(int sorted[], int sorted_size) {
+}
