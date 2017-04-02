ffea8ddb1e69804a84aad8093ea3c3551093dd0c
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 17:34:59 -0500

    Add includes for stdlib in Player.h and Player.cpp

--- Project1/Player.cpp
+++ Project1/Player.cpp
@@ -2,3 +2,21 @@
 //@AuburnUserID: mch0048
 //@filename: Player.cpp
 
+#include<Player.h>
+#include<stdlib>
+
+private int stepsRemaining;
+private int time;
+private int intelligence;
+private int money;
+
+void Player() {
+    stepsRemaining = 20;
+}
+
+void step() {
+    //use EncounterDecider class to decide the encounter
+}
+
+void searchMoney() {
+}
