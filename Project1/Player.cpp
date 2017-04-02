be0b66c435452fdc1bfa27d5192075f721adf506
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 17:47:27 -0500

    Add definition for constructor

--- Project1/Player.cpp
+++ Project1/Player.cpp
@@ -3,15 +3,25 @@
 //@filename: Player.cpp
 
 #include<Player.h>
-#include<stdlib>
+#include<random>
+
+private final int RANGE_LOWER = 8;
+private final int RANGE_UPPER = 25;
 
 private int stepsRemaining;
 private int time;
 private int intelligence;
 private int money;
 
+random_device rd;
+mt19937 gen(rd());
+uniform_real_distribution<> dis(RANGE_LOWER, RANGE_UPPER);
+
 void Player() {
     stepsRemaining = 20;
+    time = dis(gen);
+    intelligence = dis(gen);
+    money = dis(gen);
 }
 
 void step() {
