f4273fb3913b74784d62bd1eda70dd91fb2c1896
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 18:53:21 -0500

    Add header file for EncounterDecider

---
+++ Project1/EncounterDecider.h
@@ -0,0 +1,10 @@
+//@author: Maxwell Heeschen
+//@AuburnUserID: mch0048
+//@filename: DeciderEncounter.h
+
+int decideEncounter(void);
+/*
+ * Uses a random number to return what kind of encounter Player will have
+ *
+ * Returns an int representing which encounter
+ */
