c381a51d7c4294d32c2cfc49729f119c0d48becc
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 21:23:54 -0500

    Add header file for FileAccessor

---
+++ Project1/FileAccessor.h
@@ -0,0 +1,17 @@
+//@author: Maxwell Heeschen
+//@AuburnUserID: mch0048
+//@filename: FileAccessor.h
+
+#include<iostream>
+#include<fstream>
+
+void getHighScore(void);
+/*
+ * Gets the file highScores.txt and prints out the top 10 scores
+ */
+
+void changeHighScore(int score);
+/*
+ * Checks if score deserves to be in the top 10 high scores
+ */
+
