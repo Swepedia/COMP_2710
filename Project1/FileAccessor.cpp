ff923e02805aa3bb12566e163e103a86118926d9
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 22:16:53 -0500

    Add full definitions for the methods in FileAccessor

--- Project1/FileAccessor.cpp
+++ Project1/FileAccessor.cpp
@@ -10,10 +10,11 @@ using namespace std;
 
 ifstream inStream;
 ofstream outStream;
+int highScores[10];
 
 void getHighScores() {
     cout << "=====================\n";
-    cout << "     High Scores\n"
+    cout << "     High Scores\n";
     cout << "=====================\n\n";
 
     inStream.open("highScores.txt");
@@ -22,12 +23,51 @@ void getHighScores() {
         exit(1);
     }
 
-    int n = 1;
+    char next;
+    int i = 1;
     inStream.get(next);
     while(!inStream.eof()) {
-        cout << n << ". "
+        cout << i << ". ";
         cout << next << endl;
         inStream.get(next);
     }
-    inStram.close();
+    inStream.close();
+}
+
+void changeHighScore(int score) {
+    inStream.open("highScores.txt");
+    if(inStream.fail()) {
+        cout << "Failed to locate highScores.txt\n";
+        exit(1);
+    }
+
+    outStream.open("highScores.txt");
+    if(outStream.fail()) {
+        cout << "Failed to locate highScores.txt\n";
+        exit(1);
+    }
+
+
+    char next;
+    int i = 0;
+    inStream.get(next);
+    while(!inStream.eof()) {
+        highScores[i] = static_cast<int>(next);
+        i++;
+    }
+
+    for(int k = 0; k < 10; k++) {
+        if(score > highScores[k]) {
+            highScores[k] = score;
+            break;
+        }
+    }
+
+    int k = 0;
+    while(k < 10) {
+        outStream << highScores[k] << endl;
+    }
+
+    inStream.close();
+    outStream.close();
 }
