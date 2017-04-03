46e1779deb9071f747e4b0926ef76ff139fbbc6e
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 21:41:36 -0500

    Add implementation of getHighScore()

---
+++ Project1/FileAccessor.cpp
@@ -0,0 +1,33 @@
+//@author: Maxwell Heeschen
+//@AuburnUserID: mch0048
+//@filename: FileAccessor.h
+
+#include "FileAccessor.h"
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+ifstream inStream;
+ofstream outStream;
+
+void getHighScore() {
+    cout << "=====================\n";
+    cout << "     High Scores\n"
+    cout << "=====================\n\n";
+
+    inStream.open("highScores.txt");
+    if(inStream.fail()) {
+        cout << "Failed to locate highScores.txt\n";
+        exit(1);
+    }
+
+    int n = 1;
+    inStream.get(next);
+    while(!inStream.eof()) {
+        cout << n << ". "
+        cout << next << endl;
+        inStream.get(next);
+    }
+    inStram.close();
+}
