36910f01f2dc5776d66b08b124137ccc6f28296e
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-02 20:18:22 -0500

    Add implementation of EncounterDecider

--- Project1/EncounterDecider.cpp
+++ Project1/EncounterDecider.cpp
@@ -5,7 +5,24 @@
 #include<cstdlib>
 using namespace std;
 
-const double oddsNothing = 0.25;
+const int  oddsNothing = 25;
+const int oddsPuzzle = 25;
+const int oddsProfessor = 10;
+const int  oddsGradStudent = 10;
+const int oddsGruntWork = 15;
+const int  oddsGradePapers = 10;
 
 int decideEncounter(void) {
+    int odds = rand() % 100;
+    if(odds < oddsNothing)
+        return 0;
+    if(odds < oddsNothing + oddsPuzzle)
+        return 1;
+    if(odds < oddsNothing + oddsPuzzle + oddsProfessor)
+        return 2;
+    if(odds < oddsNothing + oddsPuzzle + oddsProfessor + oddsGradStudent)
+        return 3;
+    if(odds < oddsNothing + oddsPuzzle + oddsProfessor + oddsGradStudent + oddsGruntWork)
+        return 4;
+    return 5;
 }
