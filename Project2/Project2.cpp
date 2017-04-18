0f33b39736b800556dac5f9220105261b536831d
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-18 13:23:02 -0500

    Project2: initial commit

---
+++ Project2/Project2.cpp
@@ -0,0 +1,30 @@
+//@author: Maxwell Heeschen
+//@AuburnUserID: mch0048
+//@filename: Project2.cpp
+//
+//To Compile:
+//  g++ -o Project2 Project2.cpp
+//
+//Resources:
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+struct User {
+    string username;
+    string password;
+    int role;
+};
+
+struct Client {
+    string name;
+    string address;
+    int ssn;
+    string employer;
+    double income;
+};
+
+int main() {
+    return 0;
+}
