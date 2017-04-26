08be85e250589fd59835e95997de99586058416b
Maxwell Heeschen <gorpmaster@gmail.com>
2017-04-26 17:51:49 -0500

    Add basic menus for Admins and others

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -36,6 +36,18 @@ struct Client {
  *      1)Login
  *      2)Quit
  */
+int menuDecider(int upper);
+/*
+ * handles menu choices
+ */
+void screenAdmin();
+/*
+ * provides functionality for the admin screen
+ */
+void screenNormal();
+/*
+ * provides funcitonality for the non-admin screen
+ */
 
 int main() {
     cout << "================================================================\n";
@@ -44,12 +56,15 @@ int main() {
     cout << "1) Login\n";
     cout << "2) Quit\n";
     cout << "Please choose an option: ";
-    int answer = 0;
+    /*int answer = 0;
     cin >> answer;
     while(answer > 2 && answer < 1) {
         cout << "Input a valid value: ";
         cin >> answer;
     }
+    */
+    int answer;
+    answer = menuDecider(2);
     if(answer == 1) {
         ifstream input("login");
         if(input.fail()) {
@@ -64,17 +79,19 @@ int main() {
         input.close();
         string username;
         string password;
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
+        //cin.clear();
+        //cin.ignore(INT_MAX, '\n');
         bool loginSuccess = false;
+        int user = 0;
         do {
             cout << "Username: ";
             getline(cin, username);
             cout << "Password: ";
             getline(cin, password);
-            for(int i = 0; i < login.size(); i += 2) {
+            for(int i = 0; i < login.size(); i += 3) {
                 if(login[i] == username && login[i + 1] == password) {
                     loginSuccess = true;
+                    user = i;
                     break;
                 }
             }
@@ -83,8 +100,12 @@ int main() {
             }
         }
         while(!loginSuccess);
-        cout << "Success!\n";
 
+        if(stoi(login[user + 2]) == 1) {
+            screenAdmin();
+        } else {
+            screenNormal();
+        }
     } else {
         return 0;
     }
@@ -107,3 +128,38 @@ int main() {
     return answer;
 }
 */
+
+int menuDecider(int upper) {
+    int answer = 0;
+    cin >> answer;
+    while(answer > upper && answer < 1) {
+        cout << "Input a valid value: ";
+        cin >> answer;
+    }
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    return answer;
+}
+
+void screenAdmin() {
+    cout << "====================================================================\n";
+    cout << "|       Teller Terminal System - System Administration             |\n";
+    cout << "====================================================================\n";
+    cout << "1) Client and Account Management\n";
+    cout << "2) Add a branch staff member\n";
+    cout << "3) Delete a branch staff member\n";
+    cout << "4) Display branch staff\n";
+    cout << "5) Change password\n";
+    cout << "6) Exit\n";
+    int answer = menuDecider(6);
+}
+
+void screenNormal() {
+    cout << "====================================================================\n";
+    cout << "|             Teller Terminal System - Branch Staff                |\n";
+    cout << "====================================================================\n";
+    cout << "1) Client and Account Management\n";
+    cout << "2) Change password\n";
+    cout << "3) Exit\n";
+    int answer = menuDecider(3);
+}
